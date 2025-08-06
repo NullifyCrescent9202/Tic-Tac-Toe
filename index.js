029486c708b031e9798e0e6b8a3cbc036680a717
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2025-08-06 15:42:11 -0400

    Add CPU opponent.

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 function gameBoard(){
     return {
         grid: document.querySelectorAll(".grid-con > div"),
+        round: 0,
 
         getBoard(){
             console.log(this.grid);
         },
+
+        CPU(){
+            function randInt(){
+                return Math.floor(Math.random() * 9);
+            };
+            let index = randInt();
+            let cell = this.grid[index];
+            if(cell.textContent === ""){
+                cell.textContent = "O";
+            } else {
+                this.CPU();
+            }
+            this.round++;
+        },
+
         addPlayerEvents(){
             this.grid.forEach((cel) => {
                 cel.addEventListener(("click"), () =>{
                     if(cel.textContent === ""){
                         cel.textContent = "X";
-                    }
+                        this.round++;
+                        this.CPU();
+                    };
                 });
-            })
-        }
+            });
+        },
+
     };
 };
 
