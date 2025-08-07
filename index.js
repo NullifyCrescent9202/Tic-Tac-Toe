aef12caf4fc8751dfdc16e4f846f74213e46ebe3
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2025-08-06 22:38:02 -0400

    Prevent user interaction upon game over with the gameboard. Also Check win state.

--- index.js
+++ index.js
@@ -7,18 +7,64 @@ function gameBoard(){
             console.log(this.grid);
         },
 
-        CPU(){
-            function randInt(){
-                return Math.floor(Math.random() * 9);
+        winStatus(unit){
+            const gridContainer = document.querySelector(".grid-con"); 
+
+
+            function getMarker(cell){
+                return cell.textContent;
             };
-            let index = randInt();
-            let cell = this.grid[index];
-            if(cell.textContent === ""){
-                cell.textContent = "O";
-            } else {
-                this.CPU();
-            }
-            this.round++;
+
+            let grid = this.grid;
+
+            function check(cellgrid, marker, index_1, index_2, index_3){
+                if(getMarker(cellgrid[index_1]) === marker && getMarker(cellgrid[index_2]) === marker && getMarker(cellgrid[index_3]) === marker){
+                    console.log(marker + " Wins")
+                    gameOver = true;
+                    gridContainer.classList.add("block-click");
+
+
+                };
+            };
+
+
+            function getWinner(marker){
+                //Check Rows
+                check(grid, marker, 0, 1, 2);
+                check(grid, marker, 3, 4, 5);
+                check(grid, marker, 6, 7, 8);
+                
+                //Check Columns
+                check(grid, marker, 0, 3, 6);
+                check(grid, marker, 1, 4, 6);
+                check(grid, marker, 2, 5, 7);
+
+                //Check Crosses
+                check(grid, marker, 0, 4, 8);
+                check(grid, marker, 6, 4, 2);
+            };
+
+            if(gameOver != true){
+                getWinner(unit);
+            };
+
+        },
+
+        CPU(){
+            if(gameOver != true){
+                function randInt(){
+                    return Math.floor(Math.random() * 9);
+                };
+                let index = randInt();
+                let cell = this.grid[index];
+                if(cell.textContent === ""){
+                    cell.textContent = "O";
+                } else {
+                    this.CPU();
+                }
+                this.round++;
+                this.winStatus("O");
+            } else {}
         },
 
         addPlayerEvents(){
@@ -27,6 +73,7 @@ function gameBoard(){
                     if(cel.textContent === ""){
                         cel.textContent = "X";
                         this.round++;
+                        this.winStatus("X");
                         this.CPU();
                     };
                 });
@@ -37,7 +84,8 @@ function gameBoard(){
 };
 
 
-
+let gameOver = false;
 const board = gameBoard();
 board.addPlayerEvents();
 board.getBoard();
+
